d95f5253823f4331540fb87179649e4d80ea4034
hieunguyen64 <dhieu0307@gmail.com>
2021-06-24 23:12:55 +0700

    done

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace("App\Http\Controllers")->group(function () {
+    Route::prefix("user")->group(function () {
+        Route::post('register', 'UserController@register');
+        Route::post('login', 'UserController@authenticate');
+        Route::get('open', 'DataController@open');
+
+        Route::group(['middleware' => ['jwt.verify']], function () {
+            Route::get('user', 'UserController@getAuthenticatedUser');
+            Route::get('closed', 'DataController@closed');
+        });
+    });
 });
